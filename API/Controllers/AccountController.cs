2591f9e74bbd10db7f3ead563a26e2e45066010f
Bekzod Tukhtasinov <bekzod.tukhtasinov@scandiweb.com>
2024-01-09 16:50:55 +0500

    added a login endpoint

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -40,6 +40,31 @@ public class AccountController : BaseApiController
         return user;
     }
 
+    [HttpPost("login")]
+    public async Task<ActionResult<AppUser>> Login(LoginDto loginDto)
+    {
+        var user = await _context.Users.SingleOrDefaultAsync(x => x.UserName.Equals(loginDto.Username));
+
+        if (user == null)
+        {
+            return Unauthorized("Invalid username or password1");
+        }
+
+        using var hmac = new HMACSHA512(user.PasswordSalt);
+
+        var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(loginDto.Password));
+
+        for (int i = 0; i < computedHash.Length; i++)
+        {
+            if (computedHash[i] != user.PasswordHash[i])
+            {
+                return Unauthorized("Invalid username or password");
+            }
+        }
+
+        return user;
+    }
+
     private async Task<bool> UserExists(string username)
     {
         return await _context.Users.AnyAsync(x => x.UserName.Equals(username.ToLower()));
