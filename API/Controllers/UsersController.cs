67c029ca50cdf695c2d08368b6991b2455f30b79
Bekzod Tukhtasinov <bekzod.tukhtasinov@scandiweb.com>
2024-01-12 12:41:03 +0500

    used AutoMapper queryable extensions

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -1,6 +1,5 @@
 ﻿using API.DTOs;
 using API.Interfaces;
-using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -11,28 +10,22 @@ public class UsersController : BaseApiController
 {
     private readonly IUserRepository _userRepository;
 
-    private readonly IMapper _mapper;
-
-    public UsersController(IUserRepository userRepository, IMapper mapper)
+    public UsersController(IUserRepository userRepository)
     {
         _userRepository = userRepository;
-        _mapper = mapper;
     }
 
     [HttpGet]
     public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers()
     {
-        var users = await _userRepository.GetUsersAsync();
-        var usersToReturn = _mapper.Map<IEnumerable<MemberDto>>(users);
+        var users = await _userRepository.GetMembersAsync();
 
-        return Ok(usersToReturn);
+        return Ok(users);
     }
 
     [HttpGet("{username}")]
     public async Task<ActionResult<MemberDto>> GetUser(string username)
     {
-        var user = await _userRepository.GetUserByUsernameAsync(username);
-
-        return _mapper.Map<MemberDto>(user);
+        return await _userRepository.GetMemberAsync(username);
     }
 }
